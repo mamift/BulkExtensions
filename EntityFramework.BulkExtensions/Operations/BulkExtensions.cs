51735ec920c7baae9ed7ce40b630f9e322e4f470
tiagoln <tiagoln@gmail.com>
2017-04-11 17:16:36 -0300

    Updated BulkExtension methods documentation;

--- EntityFramework.BulkExtensions/Operations/BulkExtensions.cs
+++ EntityFramework.BulkExtensions/Operations/BulkExtensions.cs
@@ -4,43 +4,40 @@ using EntityFramework.BulkExtensions.BulkOperations;
 
 namespace EntityFramework.BulkExtensions.Operations
 {
-    /// <summary>
-    /// 
-    /// </summary>
     public static class BulkExtensions
     {
         /// <summary>
-        /// 
+        /// Bulk insert a collection of objects into the database.
         /// </summary>
-        /// <param name="context"></param>
-        /// <param name="entities"></param>
+        /// <param name="context">The EntityFramework DbContext object.</param>
+        /// <param name="entities">The collection of objects to be inserted.</param>
         /// <param name="identity"></param>
-        /// <typeparam name="TEntity"></typeparam>
-        /// <returns></returns>
+        /// <typeparam name="TEntity">The type of the objects collection. TEntity must be a class.</typeparam>
+        /// <returns>The number of affected rows.</returns>
         public static int BulkInsert<TEntity>(this DbContext context, IEnumerable<TEntity> entities, Identity identity = Identity.InputOnly) where TEntity : class
         {
             return OperationFactory.BulkInsert.CommitTransaction(context, entities, identity);
         }
 
         /// <summary>
-        /// 
+        /// Bulk update a collection of objects into the database.
         /// </summary>
-        /// <param name="context"></param>
-        /// <param name="entities"></param>
-        /// <typeparam name="TEntity"></typeparam>
-        /// <returns></returns>
+        /// <param name="context">The EntityFramework DbContext object.</param>
+        /// <param name="entities">The collection of objects to be updated.</param>
+        /// <typeparam name="TEntity">The type of the objects collection. TEntity must be a class.</typeparam>
+        /// <returns>The number of affected rows.</returns>
         public static int BulkUpdate<TEntity>(this DbContext context, IEnumerable<TEntity> entities) where TEntity : class
         {
             return OperationFactory.BulkUpdate.CommitTransaction(context, entities);
         }
 
         /// <summary>
-        /// 
+        /// Bulk delete a collection of objects from the database.
         /// </summary>
-        /// <param name="context"></param>
-        /// <param name="entities"></param>
-        /// <typeparam name="TEntity"></typeparam>
-        /// <returns></returns>
+        /// <param name="context">The EntityFramework DbContext object.</param>
+        /// <param name="entities">The collection of objects to be deleted.</param>
+        /// <typeparam name="TEntity">The type of the objects collection. TEntity must be a class.</typeparam>
+        /// <returns>The number of affected rows.</returns>
         public static int BulkDelete<TEntity>(this DbContext context, IEnumerable<TEntity> entities) where TEntity : class
         {
             return OperationFactory.BulkDelete.CommitTransaction(context, entities);
